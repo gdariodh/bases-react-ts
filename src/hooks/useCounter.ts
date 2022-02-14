c6e07788504e559c0d708fd3d24a19f566ce7d9b
Gabriel Diaz <diazhernandezgabriel2001@gmail.com>
2022-02-14 17:41:57 -0300

    params to custom hook

--- src/hooks/useCounter.ts
+++ src/hooks/useCounter.ts
@@ -1,40 +1,45 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, useLayoutEffect } from "react";
 // libreria animaciones
 import { gsap } from "gsap";
 
-const MAXIMUN_COUNT = 10;
-
-export const useCounter = () => {
+export const useCounter = ({ maxCount = 5 }) => {
   const [counter, setCounter] = useState(5);
 
-  const counterElement = useRef<HTMLHeadingElement>(null);
+  const elementToAnimate = useRef<any>(null);
+
+  const tl = useRef(gsap.timeline()).current; // .current para acceder a la referencia directa y no al objeto q devuelve useRef
 
   const handleClick = () => {
     // Math.min es para poder sumar el state +1 hasta llegar a 10 que el maximo y no pasa, es como un rango de valores numericos
     // maximo 10
-    setCounter((previousValue) => Math.min(previousValue + 1, MAXIMUN_COUNT));
+    setCounter((previousValue) => Math.min(previousValue + 1, maxCount));
   };
 
-  useEffect(() => {
-    // animacion con gsap
-    const tl = gsap.timeline();
+  useLayoutEffect(() => {
+    if (!elementToAnimate.current) return;
 
-    tl.to(counterElement.current, {
+    // animacion con gsap
+
+    tl.to(elementToAnimate.current, {
       y: -10,
       duration: 0.2,
       ease: "ease.out",
-    }).to(counterElement.current, {
+    }).to(elementToAnimate.current, {
       y: 0,
       duration: 1,
       ease: "bounce.out",
-    });
+    }).pause()
 
     // .current es para acceder al elemento html q en este caso es el h2
+  }, []);
+
+  useEffect(() => {
+      tl.play()
   }, [counter]);
 
   return {
     counter,
-    counterElement,
+    elementToAnimate,
     handleClick,
   };
 };
