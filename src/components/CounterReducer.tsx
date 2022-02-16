e074121e1d844ad0b8948dcfec8d727c316a913b
Gabriel Diaz <diazhernandezgabriel2001@gmail.com>
2022-02-15 22:43:09 -0300

    added CounterAction and CounterReducer

--- src/components/CounterReducer.tsx
+++ src/components/CounterReducer.tsx
@@ -9,21 +9,44 @@ interface CounterState {
 
 // colocamos la interface pa hacerlo estricto el state
 const INITIAL_STATE: CounterState = {
-  counter: 0,
-  previous: 0,
-  changes: 0,
+  counter: 10,
+  previous: 15,
+  changes: 20,
 };
 
+//  actions and types con Typescript way
+type CounterAction = 
+   | { type: 'increaseBy', payload: {value: number}}
+   | { type: 'reset' }
+
+
+// reducer
+const CounterReducer = (state: CounterState, action: CounterAction): CounterState => {
+  switch (action.type) {
+      case 'reset':
+          return{ 
+            counter: 0,
+            previous: 0,
+            changes: 0
+        }
+  
+      default:
+          return state;
+  }
+}
+
 export const CounterReducerComponent = () => {
-  const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
+  const [state, dispatch] = useReducer(CounterReducer, INITIAL_STATE);
 
   const handleClick = () => {
-    setCounter((previousValue) => previousValue + 1);
+    dispatch({
+        type: 'reset'
+    })
   };
 
   return (
     <>
-      <h1>Counter Reducer: {counter}</h1>
+      <h1>Counter Reducer: {state.counter}</h1>
 
       <button onClick={handleClick}>+1 </button>
     </>
